9901e30a32c16ef0f954f19c9238a9a2126203d7
pafz <paferza@gmail.com>
2023-10-10 20:50:40 +0200

    API url corrected

--- main.js
+++ main.js
@@ -1,43 +1,46 @@
 const btnSubmit = document.getElementById('submit');
 const btnReset = document.getElementById('reset');
 const container = document.getElementById('film-info');
-console.log(container);
+const titleSearch = document.getElementById('title');
 
 const startAPI = async e => {
   e.preventDefault();
 
-  let res = [];
-  console.log('hi');
-
   try {
-    res = await axios.get(
-      'https://api.themoviedb.org/3/movie/550?api_key=600dc298bedbd11d25118262a343371d'
+    const res = await axios.get(
+      'https://api.themoviedb.org/3/search/movie?query=' +
+        titleSearch.value +
+        '&include_adult=false&language=en-US&page=1&api_key=600dc298bedbd11d25118262a343371d'
     );
+    console.log(res);
+    console.log(res.data.results.length);
+    let i = 0;
+    while (i < res.data.results.length) {
+      const resPoster = res.data.results[i].poster_path;
+      const resTitle = res.data.results[i].original_title;
+      const resGenre = res.data.results[i].genre_ids;
+      const resOverview = res.data.results[i].overview;
 
-    const resPoster = res.data.poster_path;
-    const resTitle = res.data.original_title;
-    const resGenre = res.data.genres[0].name;
-    const resOverview = res.data.overview;
+      const div = document.createElement('div');
+      container.appendChild(div);
 
-    const div = document.createElement('div');
-    console.log(container);
-    container.appendChild(div);
+      let img = document.createElement('img');
+      img.setAttribute('src', 'https://image.tmdb.org/t/p/w500/' + resPoster);
+      div.appendChild(img);
 
-    let img = document.createElement('img');
-    img.setAttribute('src', 'https://image.tmdb.org/t/p/w500/' + resPoster);
-    div.appendChild(img);
+      let h4 = document.createElement('h4');
+      h4.innerHTML = resGenre;
+      div.appendChild(h4);
 
-    let h4 = document.createElement('h4');
-    h4.innerHTML = resGenre;
-    div.appendChild(h4);
+      let h1 = document.createElement('h1');
+      h1.innerText = resTitle;
+      div.appendChild(h1);
 
-    let h1 = document.createElement('h1');
-    h1.innerText = resTitle;
-    div.appendChild(h1);
-
-    let p = document.createElement('p');
-    p.innerHTML = resOverview;
-    div.appendChild(p);
+      let p = document.createElement('p');
+      p.innerHTML = resOverview;
+      div.appendChild(p);
+      i++;
+    }
   } catch (err) {
     console.error(err);
   }
@@ -45,7 +48,6 @@ const startAPI = async e => {
 
 const resetApi = async e => {
   e.preventDefault();
-  console.log('hi');
   try {
     const empty = e => {
       while (e.firstElementChild) {
